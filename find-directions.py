dce16240706a29d1e3604725e1a8c3c94b393abb
andreass0n <132298701+andreass0n@users.noreply.github.com>
2023-05-20 09:53:49 +0200

    Update find-directions.py
    Changed the orientation calculation from relative to North to relative to East due to easier plotting.
    Now writes orientation data into Excel file with all relevant information.

--- find-directions.py
+++ find-directions.py
@@ -1,5 +1,6 @@
+import pandas as pd
+import os
 import numpy as np
-import matplotlib.pyplot as plt
 import math
 
 # Order of points is:
@@ -14,66 +15,63 @@ import math
 # 8 = base of tail (magenta)
 # 9 = tip of tail (turqoise)
 
-# frame 0 of numpy array corresponds to what time in what video:
-# 015-01 // frame 0 = 2:47m or 167s
-# 083-01 // frame 0 = 0:43 or 43s
+# Defining parameters
+observation = '083'  
+bout_id = observation + '.01'   # 01 = foraging, 02 = resting, 03 = reacting
+behavior = 'foraging'
+species = 'Grevys_zebra'
+video_start_time = 286          # Start time of segment in video (in seconds)
+video_end_time = 400            # End time of segment in video (in seconds)
+start_frame_num = 2576          # Start frame number of video (where numpy frame 0 corresponds to)
+video_frame_rate = 60
+numpy_frame_rate = 30
+stepsize = 30                   # Only include every stepsize'th frame
 
+# Calculate corresponding start and end frames in numpy array of segment
+start_frame = int((video_start_time * video_frame_rate - start_frame_num) / (video_frame_rate / numpy_frame_rate))
+end_frame = int((video_end_time * video_frame_rate - start_frame_num) / (video_frame_rate / numpy_frame_rate))
 
-# Variables (add array_relation_constant (time in seconds into the video when array frame 0 starts (look up in table above)) // add timewindow in seconds from start of video of specific segment)
+# Load posture data
+postures = np.load(f'posture_data_utms/observation{observation}-postures.npy')
 
-frame_rate = 30
-array_relation_constant = 167
-start_time = 370
-end_time = 371
-
-# Convert start/end time of segment to start of array frames
-
-start_frame = int((start_time - array_relation_constant) * frame_rate)
-end_frame = int((end_time - array_relation_constant) * frame_rate)
-
-
-# Load array and extract neck and tail keypoints of frames of interest
-
-postures = np.load('posture_data_utms/observation015-postures.npy')
-
-neck_points = postures[:, start_frame:end_frame, 3]
-tail_points = postures[:, start_frame:end_frame, 8]
-
-
-# Create function to calculate all orientation angles relative to true north of all animals in specific segment 
-
-def angle_from_north(tail, neck):
-
-    # Calculate differences in easting and northing of neck and tail
-
+# Define function to calculate angles from EAST
+def angle_from_east(tail, neck):
     delta_easting = neck[0] - tail[0]
     delta_northing = neck[1] - tail[1]
-
-
-    # Calculate angle of the vector between tail and neck points relative to east
-
     angle_rad = math.atan2(delta_northing, delta_easting)
-
-
-    # Convert angle from radians to degrees
-
     angle_deg = math.degrees(angle_rad)
+    return (angle_deg + 360) % 360
 
+# Extract key points and calculate angles
+neck_points = postures[:, start_frame:end_frame:stepsize, 3]
+tail_points = postures[:, start_frame:end_frame:stepsize, 8]
+angles_from_east = [[round(angle_from_east(tail_points[i, j], neck_points[i, j]), 1) for j in range(tail_points.shape[1])] for i in range(tail_points.shape[0])]
 
-    # Convert angle relative to east to angle relative to north
-
-    angle_from_north = (90 - angle_deg) % 360
-    return angle_from_north
-
-
-# Run function for every animal; store results in list
-
-angles_from_north = [[angle_from_north(tail_points[i, j], neck_points[i, j]) for j in range(tail_points.shape[1])] for i in range(tail_points.shape[0])]
-
-
-# Print results
-
-for i, animal_angles in enumerate(angles_from_north):
+# Prepare data for dataframe
+data = []
+for i, animal_angles in enumerate(angles_from_east):
     for j, angle in enumerate(animal_angles):
-        print(f"Animal {i+1} in frame {start_frame + j}: Angle relative to North in degrees: {angle:.1f}")
-    print()
+        if not math.isnan(angle):
+            numpy_frame_num = start_frame + j * stepsize
+            frame_num = observation + '-' + str(numpy_frame_num)
+            individual_id = observation + '-' + str(i)
+            data.append([observation, bout_id, behavior, species, individual_id, frame_num, angle])
+
+# Create dataframe
+df = pd.DataFrame(data, columns=['observation', 'bout_id', 'behavior', 'species', 'individual_id', 'frame_num', 'orientation'])
+
+# Save DataFrame to excel file
+output_filename = 'behavior_data.xlsx'
+if os.path.exists(output_filename):
+    df_existing = pd.read_excel(output_filename, dtype=np.object_)
+    df_combined = pd.concat([df_existing, df])
+    duplicates = df_combined[df_combined.duplicated(keep='last')]
+    if not duplicates.empty:
+        print("Duplicates found:")
+        for idx, row in duplicates.iterrows():
+            print(f"Row {idx+2}: {row.tolist()}")
+        print("Cannot add duplicate entries.")
+    else:
+        df_combined.to_excel(output_filename, index=False)
+else:
+    df.to_excel(output_filename, index=False)
